b8f85b6481365ec39cf72fa79ec2f5d71c50979d
dunowljj <uuufg3322@gmail.com>
2022-01-04 22:50:14 +0900

    commit social login

---
+++ src/main/java/com/lduldu/book/springboot/domain/user/SessionUser.java
@@ -0,0 +1,19 @@
+package com.lduldu.book.springboot.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
